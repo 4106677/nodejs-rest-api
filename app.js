87cdde41b2d21b6b11ca9b122b8185ec3d1a52fc
Yaroslav Oliinyk <4106677@gmail.com>
2023-02-13 19:22:48 +0200

    fix privat DB URL

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const logger = require('morgan');
 const cors = require('cors');
 const mongoose = require('mongoose');
 // ftVRKKC8886mD18m
-const DB_HOST = 'mongodb+srv://admin:ftVRKKC8886mD18m@contacts.a2nwyrl.mongodb.net/node?retryWrites=true&w=majority'
+const {DB_HOST} = require('./config')
 mongoose.connect(DB_HOST)
     .then(()=> console.log('Data Base Connect'))
     .catch(err => {
---
+++ config.js
@@ -0,0 +1,5 @@
+const DB_HOST = 'mongodb+srv://admin:ftVRKKC8886mD18m@contacts.a2nwyrl.mongodb.net/node?retryWrites=true&w=majority';
+
+module.exports = {
+	DB_HOST
+}
